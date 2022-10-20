90ef57d24e29c30530b62beea020f3ca947e2049
yifeiwu <wyf990505@gmail.com>
2022-10-20 11:46:45 -0400

    mock cc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 // write your code here
+
+
+fetch ('http://localhost:3000/ramens') //get data from server
+.then(resp => resp.json())
+.then(data => { data.forEach(elem =>{
+    //forEach to choose everything from json data
+    const ramenImg = document.querySelector('#ramen-menu')
+    const img = document.querySelector('#ramen-detail')
+    //create variables to grab the two sections
+    document.h2.append(ramenImg, img)
+    //give them home
+    img.src = data.img;
+    // put content in the created element,display image
+    }) 
+})
+
+const img = document.querySelector('#ramen-detail')
+
+img.addEventListener('click', () => {
+        e.preventDefault()
+    
+        document.body.append(img)
+        
+})
+//display all ramen images, click to show specific image 
